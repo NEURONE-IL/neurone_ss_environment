56ccbc280b9b3202606424b2ecde62f229edb873
Mateo Sepúlveda <mateo.sepulveda@usach.cl>
2022-04-15 19:30:56 -0400

    Simulation date format fixed, using Day.js library

--- nsi/src/app/components/simulation-list/simulation-list.component.ts
+++ nsi/src/app/components/simulation-list/simulation-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-simulation-list',
@@ -13,10 +14,10 @@ export class SimulationListComponent {
   columns: string[] = ['name', 'creator', 'creationDate', 'lastDeployDate', 'numberStudents'];
 
   data: Simulation[] = [
-    new Simulation('Simulation1', 'John', new Date(2015, 11, 17, 13, 24, 2), new Date(2015, 11, 18, 9, 8, 5), 240),
-    new Simulation('Simulation2', 'George', new Date(2016, 11, 1, 15, 2, 10), new Date(2016, 11, 3, 11, 59, 55), 120),
-    new Simulation('Simulation3', 'Paul', new Date(2018, 11, 23, 8, 51, 55), new Date(2018, 11, 24, 12, 9, 31), 80),
-    new Simulation('Simulation3', 'Ringo', new Date(2018, 11, 23, 9, 36, 33), new Date(2018, 11, 25, 17, 45, 24), 100)
+    new Simulation('Simulation1', 'John', dayjs(new Date(2015, 11, 17, 13, 24, 2)).format('YYYY/MM/DD HH:mm:ss'), dayjs(new Date(2015, 11, 18, 9, 8, 5)).format('YYYY/MM/DD HH:mm:ss'), 240),
+    new Simulation('Simulation2', 'George', dayjs(new Date(2016, 11, 1, 15, 2, 10)).format('YYYY/MM/DD HH:mm:ss'), dayjs(new Date(2016, 11, 3, 11, 59, 55)).format('YYYY/MM/DD HH:mm:ss'), 120),
+    new Simulation('Simulation3', 'Paul', dayjs(new Date(2018, 11, 23, 8, 51, 55)).format('YYYY/MM/DD HH:mm:ss'), dayjs(new Date(2018, 11, 24, 12, 9, 31)).format('YYYY/MM/DD HH:mm:ss'), 80),
+    new Simulation('Simulation3', 'Ringo', dayjs(new Date(2018, 11, 23, 9, 36, 33)).format('YYYY/MM/DD HH:mm:ss'), dayjs(new Date(2018, 11, 25, 17, 45, 24)).format('YYYY/MM/DD HH:mm:ss'), 100)
   ];
 
   dataSource: any;
@@ -31,7 +32,7 @@ export class SimulationListComponent {
 
 export class Simulation {
 
-  constructor(public name: string, public creator: string, public creationDate: Date, public lastDeployDate: Date, public numberStudents: number) {
+  constructor(public name: string, public creator: string, public creationDate: string, public lastDeployDate: string, public numberStudents: number) {
   }
 
 }
